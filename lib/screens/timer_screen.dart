154398b2916e4dbc6b5ea4836c1509f6995a7b0c
CY <cksdud8028@naver.com>
2022-09-27 10:32:30 +0900

    made main screen

---
+++ lib/screens/timer_screen.dart
@@ -0,0 +1,79 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatefulWidget {
+  @override
+  _TimerScreen createState() => _TimerScreen();
+}
+
+class _TimerScreen extends State<TimerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> _runningButtons = [
+      ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          1 == 2 ? '계속하기' : '일시정지',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+      ),
+      Padding(padding: EdgeInsets.all(20)),
+      ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          '포기하기',
+          style: TextStyle(fontSize: 16),
+        ),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+      ),
+    ];
+
+    final List<Widget> _stoppedButtons = [
+      ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          '시작하기',
+          style: TextStyle(color: Colors.white),
+        ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: 1 == 2 ? Colors.green : Colors.blue),
+      )
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('뽀모도로 타이머'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: Center(
+              child: Text(
+                '00:00',
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: 1 == 2 ? Colors.green : Colors.blue),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: 1 == 2
+                ? const []
+                : 1 == 2
+                    ? _stoppedButtons
+                    : _runningButtons,
+          )
+        ],
+      ),
+    );
+  }
+}
